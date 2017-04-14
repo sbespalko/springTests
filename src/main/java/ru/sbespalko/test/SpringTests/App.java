228e0395919c9441b904c65293761f7374720120
user <user@21-000851>
2017-04-14 14:51:53 +0300

    init commit true

--- src/main/java/ru/sbespalko/test/SpringTests/App.java
+++ src/main/java/ru/sbespalko/test/SpringTests/App.java
@@ -4,8 +4,10 @@ package ru.sbespalko.test.SpringTests;
  * Hello world!
  *
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
