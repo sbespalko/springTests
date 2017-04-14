c1cc388c4241456b567e7f9d8632714c87f0bb84
user <user@21-000851>
2017-04-14 15:47:28 +0300

    pom edit

--- src/main/java/ru/sbespalko/test/SpringTests/App.java
+++ src/main/java/ru/sbespalko/test/SpringTests/App.java
@@ -4,10 +4,8 @@ package ru.sbespalko.test.SpringTests;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
